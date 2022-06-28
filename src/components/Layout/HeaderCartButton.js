123c23aeba944f0ee6b1c6eb73d92a5dfa2c3399
Rana_awais <z.manzoor@pioneerlogics.com>
2022-06-28 03:03:03 -0700

    bumping cart button with useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 function HeaderCartButton(props) {
-  const cartCtx = useContext(CartContext);
+  //useState for button bump functionalities
+  const [btnIsHighlighted, setBtnIsHighLighted] = useState(false);
 
-  const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
+  //Context provider
+  const cartCtx = useContext(CartContext);
+  //object destructuring
+  const { items } = cartCtx;
+  //total number of items in cart
+  const numberOfItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+
+  //add classes to button
+  const btnClasses = ` ${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  //useEffect
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighLighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+  ////////////
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
